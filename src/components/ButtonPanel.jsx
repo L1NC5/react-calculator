fcb9ee94a61f6e2a6a9ffb3dbaa7fae676dafffb
Federico Barbieri <federico.barbieri@grupposcai.it>
2024-05-25 00:43:42 +0200

    Handle decimals and operators, create buttonpanel

---
+++ src/components/ButtonPanel.jsx
@@ -0,0 +1,15 @@
+function ButtonPanel() {
+	const buttons = [
+		[
+			{label: 'C', value: 'C', callback:''},
+			{label: '+/-', value: '+/-', callback: ''}
+		]
+	]
+  return (
+    <>
+      <p>Pippo</p>
+    </>
+  );
+}
+
+export default ButtonPanel;
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,61 +1,82 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ButtonPanel from "./ButtonPanel";
 
 function Calculator() {
   const [previousNumber, setPreviousNumber] = useState("0");
   const [currentNumber, setCurrentNumber] = useState("0");
-  const [currentOperator, setCurrentOperator] = useState("");
-  let activeOperator = false;
+  const [currentOperator, setCurrentOperator] = useState("+");
+  const [operationState, setOperationState] = useState("false");
+  const [decimalState, setDecimalState] = useState(false);
 
   const switchOperator = (operator) => {
+    setDecimalState(false);
     if (currentOperator !== "") {
       let result = 0;
       switch (currentOperator) {
         case "+":
           result = String(
-            parseInt(previousNumber, 10) + parseInt(currentNumber, 10),
+            parseFloat(previousNumber, 10) + parseFloat(currentNumber, 10),
           );
           break;
         case "-":
           result = String(
-            parseInt(previousNumber, 10) - parseInt(currentNumber, 10),
+            parseFloat(previousNumber, 10) - parseFloat(currentNumber, 10),
           );
           break;
         case "/":
           result = String(
-            parseInt(previousNumber, 10) / parseInt(currentNumber, 10),
+            parseFloat(previousNumber, 10) / parseFloat(currentNumber, 10),
           );
           break;
         case "*":
           result = String(
-            parseInt(previousNumber, 10) * parseInt(currentNumber, 10),
+            parseFloat(previousNumber, 10) * parseFloat(currentNumber, 10),
           );
           break;
       }
       setPreviousNumber(result);
-      if (operator === "=") {
-        setCurrentNumber(result);
-        activeOperator = false;
-      } else {
-        setCurrentNumber("0");
-        activeOperator = true;
-      }
+      operator === "=" ? setCurrentNumber(result) : setCurrentNumber("0");
     }
-    switch (operator) {
-      default:
-        console.log("operator = 0");
+    if (operator === "=") {
+      setCurrentOperator("");
+      setOperationState(false);
+    } else {
+      setCurrentOperator(operator);
+      setOperationState(true);
     }
   };
 
+  const handleDecimalState = () => {
+    if (decimalState == false) {
+      setDecimalState(true);
+      setCurrentNumber(currentNumber + '.');
+    } else {
+    	console.log('Decimale già inserito!');
+    }
+  };
+
+  // UseEffect: check for changes in the declared variable [name] and fire a function accordingly
+  useEffect(() => {
+    console.log(operationState);
+  }, [operationState]);
+
   return (
-    <>
-    	{!activeOperator && <p>Ciao</p>}
+    <div className="bg-slate-800 p-4 rounded-lg">
+      {!operationState && <p>Ciao</p>}
+      <ButtonPanel />
       <button
-        className={`bg-white text-black p-4 ${activeOperator && 'border-2 border-red-600'}`}
+        className={`bg-white text-black p-4 ${operationState && "border-2 border-red-600"}`}
         onClick={() => switchOperator("=")}
       >
-        Test
+        =
       </button>
-    </>
+      <button
+        className={`bg-white text-black p-4 ${operationState && "border-2 border-red-600"}`}
+        onClick={() => switchOperator("+")}
+      >
+        +
+      </button>
+    </div>
   );
 }
 
